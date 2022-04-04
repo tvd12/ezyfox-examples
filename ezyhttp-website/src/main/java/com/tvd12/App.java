0d7d5e60232fd80adb29738106017eacf5f36c41
conght11 <conght11@ghtk.co>
2022-04-04 21:40:03 +0700

    test project

---
+++ ezyhttp-website/src/main/java/com/tvd12/App.java
@@ -0,0 +1,12 @@
+package com.tvd12;
+
+import com.tvd12.ezyhttp.core.boot.EzyHttpApplicationBootstrap;
+import com.tvd12.ezyhttp.server.core.asm.RequestHandlerImplementer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        RequestHandlerImplementer.setDebug(true);
+        EzyHttpApplicationBootstrap.start(App.class);
+    }
+
+}
