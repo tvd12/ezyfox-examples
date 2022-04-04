0ac36d2c403f9a3b087234aecdc90bb2fdb6bd2b
conght11 <conght11@ghtk.co>
2022-04-04 21:38:05 +0700

    update call http and http proxy

--- ezy-http/src/main/java/com/tvd12/user_management/test/ApiAddUserTest.java
+++ ezy-http/src/main/java/com/tvd12/user_management/test/ApiAddUserTest.java
@@ -1,19 +1,18 @@
 package com.tvd12.user_management.test;
 
+import com.tvd12.ezyhttp.client.HttpClient;
 import com.tvd12.ezyhttp.client.request.PostRequest;
 import com.tvd12.ezyhttp.client.request.Request;
 import com.tvd12.ezyhttp.client.request.RequestEntity;
 import com.tvd12.ezyhttp.core.constant.StatusCodes;
 import com.tvd12.user_management.entity.User;
 
-import java.net.http.HttpClient;
-import java.net.http.HttpResponse;
 
 public class ApiAddUserTest {
     public static final String API_URL = "http://localhost:8080/api/v1/users/";
 
-    public static void main(String[] args) {
-        HttpClient httpClient = HttpClient.newBuilder().build();
+    public static void main(String[] args) throws Exception {
+        HttpClient httpClient = HttpClient.builder().build();
         User body = new User();
         body.setUsername("tvd12");
         body.setPassword("123456");
@@ -23,8 +22,8 @@ public class ApiAddUserTest {
                 .setEntity(requestEntity)
                 .setResponseType(Boolean.class)
                 .setResponseType(StatusCodes.CONFLICT,String.class);
-        Boolean response = httpClient.send(request, HttpResponse.getBo<Boolean.class>);
-
+        Boolean response = httpClient.call(request);
+        System.out.println("add user reponse: " + response);
 
     }
 }
