e1f052fdd320ee7f2d8b47aa668a02f4f7469002
Dung Ta Van <itprono3@gmail.com>
2022-04-20 22:37:30 +0700

    update ezyhttp to v1.0.2

--- ezyhttp-website/src/main/java/org/youngmonkeys/example/ezyhttp/website/App.java
+++ ezyhttp-website/src/main/java/org/youngmonkeys/example/ezyhttp/website/App.java
@@ -1,6 +1,6 @@
 package org.youngmonkeys.example.ezyhttp.website;
 
-import com.tvd12.ezyhttp.core.boot.EzyHttpApplicationBootstrap;
+import com.tvd12.ezyhttp.server.boot.EzyHttpApplicationBootstrap;
 import com.tvd12.ezyhttp.server.core.asm.RequestHandlerImplementer;
 
 public class App {
