b88df9625d6d93a98e9b26794abd599cc2c53629
conght11 <conght11@ghtk.co>
2022-05-18 16:14:09 +0700

    tach controller

--- ezyhttp-website/src/main/java/org/youngmonkeys/example/ezyhttp/website/user_management/controller/UserController.java
+++ ezyhttp-website/src/main/java/org/youngmonkeys/example/ezyhttp/website/user_management/controller/UserController.java
@@ -1,60 +1,29 @@
 package org.youngmonkeys.example.ezyhttp.website.user_management.controller;
 
 import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
-import com.tvd12.ezyhttp.server.core.annotation.*;
-import com.tvd12.ezyhttp.server.core.request.RequestArguments;
+import com.tvd12.ezyhttp.server.core.annotation.Controller;
+import com.tvd12.ezyhttp.server.core.annotation.DoPost;
+import com.tvd12.ezyhttp.server.core.annotation.RequestBody;
 import com.tvd12.ezyhttp.server.core.view.Redirect;
-import com.tvd12.ezyhttp.server.core.view.View;
 import lombok.Setter;
 import org.youngmonkeys.example.ezyhttp.website.user_management.entity.User;
 import org.youngmonkeys.example.ezyhttp.website.user_management.service.UserService;
 
 @Setter
-@Controller("/users")
+@Controller("api/v1/users")
 public class UserController {
+
     @EzyAutoBind
     protected UserService userService;
 
-    @DoGet("/add")
-    public View addUserView() {
-        return View.builder()
-            .template("user-add")
-            .build();
-    }
-
-    @DoGet("/search")
-    public View searchUserView() {
-        return View.builder()
-            .template("user-search")
-            .build();
-    }
 
     @DoPost("/add")
     public Redirect addUser(@RequestBody User user) {
         userService.addUser(user);
-        return Redirect.builder()
-            .uri("/api/v1/users/home")
-            .addAttribute("username", user.getUsername())
-            .build();
+        return Redirect.builder().uri("/users/home")
+                .addAttribute("username",user.getUsername())
+                .build();
     }
 
-    @DoGet("/home")
-    public Object home(RequestArguments arguments) {
-        String username = arguments.getRedirectionAttribute("username");
-        return View.builder()
-            .addVariable("username", username)
-            .template("home")
-            .build();
-    }
-
-    @DoGet("/{username}")
-    public View getUser(@PathVariable("username") String username) {
-        User user = userService.getUser(username);
-        String msg = user != null ? "User " + username + " with info: " + user : "User " + username + " not found";
-        return View.builder()
-            .addVariable("message", msg)
-            .template("user-info")
-            .build();
-    }
 
 }
